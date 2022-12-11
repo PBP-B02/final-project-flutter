6c82e41ad3c824d38e456a9e12f29ce35b4bb282
Failasuf Indi Marsendy <failasufindi123@gmail.com>
2022-12-12 02:15:12 +0700

    json handling

---
+++ lib/models/article.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Article {
+  final int id;
+  final String title;
+  final String author;
+  final String content;
+
+  const Article({
+    required this.id,
+    required this.author,
+    required this.title,
+    required this.content,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
+        id: json['id'],
+        author: json['author'],
+        title: json['title'],
+        content: json['content'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'author': author,
+        'urlImage': content,
+      };
+}
---
+++ lib/widget/search_article.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatefulWidget {
+  final String text;
+  final ValueChanged<String> onChanged;
+  final String hintText;
+
+  const SearchWidget({
+    Key? key,
+    required this.text,
+    required this.onChanged,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  _SearchWidgetState createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final styleActive = TextStyle(color: Color(0xFF5DB075));
+    final styleHint = TextStyle(color: Colors.black54);
+    final style = widget.text.isEmpty ? styleHint : styleActive;
+
+    return Container(
+      height: 42,
+      margin: const EdgeInsets.fromLTRB(16, 30, 16, 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Color(0xFFE7F3EA),
+        border: Border.all(color: Color(0xFF5DB075)),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          icon: Icon(Icons.search, color: style.color),
+          suffixIcon: widget.text.isNotEmpty
+              ? GestureDetector(
+                  child: Icon(Icons.close, color: style.color),
+                  onTap: () {
+                    controller.clear();
+                    widget.onChanged('');
+                    FocusScope.of(context).requestFocus(FocusNode());
+                  },
+                )
+              : null,
+          hintText: widget.hintText,
+          hintStyle: style,
+          border: InputBorder.none,
+        ),
+        style: style,
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
